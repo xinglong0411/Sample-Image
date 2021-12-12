0a1de04badda4330a2553ea4893c0aaa6e1e3eef
Dennis Zheng <xinzhen@microsoft.com>
2021-12-12 15:31:20 +0800

    First Commit

---
+++ AcrHelloworld/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Collections;
+
+namespace AcrHelloworld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var registryURL = Environment.GetEnvironmentVariable("DOCKER_REGISTRY");
+                ViewData["REGISTRYURL"] = registryURL;
+                if (registryURL != "<acrName>.azurecr.io")
+                {
+                    var hostEntry = await System.Net.Dns.GetHostEntryAsync(registryURL);
+                    ViewData["HOSTENTRY"] = hostEntry.HostName;
+
+                    string region = hostEntry.HostName.Split('.')[1];
+                    ViewData["REGION"] = region;
+
+                    var registryIp = System.Net.Dns.GetHostAddresses(registryURL)[0].ToString();
+                    ViewData["REGISTRYIP"] = registryIp;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return View();
+        }
+    }
+}
